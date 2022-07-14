882883b555b4d5014e4987bc31846977cfda7a77
shaikh3533 <hamza.shaikh3533@gmail.com>
2022-07-14 15:38:45 +0500

    Login module added

--- src/Routes/Login/index.js
+++ src/Routes/Login/index.js
@@ -15,47 +15,49 @@ const index = (props) => {
     };
     return (
         <body className='themeContainer items_middle'>
-            <form onSubmit={props.login} className='col-12 col-md-6 col-lg-4 mx-auto'>
-                <div className='text-center'>
-                    <img src={logo} className="logo_size mx-auto" />
-                    <h3 className='my-2 mx-auto fw-bold theme_text'>Login Here</h3>
+            <form onSubmit={props.login} className='col-12 col-md-6 col-lg-4 mx-auto min_height'>
+                <div className='my-auto mx-auto'>
+                    <div className='text-center'>
+                        <img src={logo} className="logo_size mx-auto" />
+                        <h3 className='my-2 mx-auto fw-bold theme_text'>Login Here</h3>
+                    </div>
+
+                    <FormControl className='w-100 mt-3' variant="outlined">
+                        <TextField
+                            id="outlined-basic"
+                            label="Email / Username"
+                            name="user_id"
+                            variant="outlined"
+                            clasName='input_'
+                            onChange={props.handleFormChange} />
+                    </FormControl>
+
+                    <FormControl className='w-100 mt-3' variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={!isHidden ? 'text' : 'password'}
+                            name="user_password"
+                            // value={props.user_password}
+                            onChange={props.handleFormChange}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {isHidden ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+                    <p className='mb-0'>(admin  123)</p>
+                    <button className='button_'>Log In <LoginIcon className='ms-2' /></button>
                 </div>
-
-                <FormControl className='w-100 mt-3' variant="outlined">
-                    <TextField
-                        id="outlined-basic"
-                        label="Email / Username"
-                        name="user_id"
-                        variant="outlined"
-                        clasName='input_'
-                        onChange={props.handleFormChange} />
-                </FormControl>
-
-                <FormControl className='w-100 mt-3' variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        type={!isHidden ? 'text' : 'password'}
-                        name="user_password"
-                        // value={props.user_password}
-                        onChange={props.handleFormChange}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {isHidden ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Password"
-                    />
-                </FormControl>
-                <p className='mb-0'>(admin  123)</p>
-                <button className='button_'>Log In <LoginIcon className='ms-2' /></button>
             </form>
         </body>
     )
