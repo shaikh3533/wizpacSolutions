e78cf0c22236bfd5b92d98b30f21d4a2aa33de1d
shaikh3533 <hamza.shaikh3533@gmail.com>
2022-07-20 14:28:55 +0500

    small change to lonin rout

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
     return isLoggedin;
   }
 
-  var loginState = checkLogin();
+  const loginState = checkLogin();
 
   useEffect(() => {
     const screenWidth = window.screen.width;
