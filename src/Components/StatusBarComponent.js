b4e1c2f6bfe6fb88e3e0dd9e0dcc9d2a4883df32
ranamohsin008 <ranamohsinrehman008@gmail.com>
2022-07-20 18:11:13 +0500

    status bar

---
+++ src/Components/StatusBarComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StatusBarComponent() {
+  return (
+     <p className='mt-2 ms-1'> hellow </p>
+  )
+}
--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -15,15 +15,11 @@ import { MenuModule } from "@ag-grid-enterprise/menu";
 import { FiltersToolPanelModule } from "@ag-grid-enterprise/filter-tool-panel";
 import "ag-grid-enterprise";
 import "./TableComponent.css";
-import {
-  Clear,
-  Search,
-} from "@material-ui/icons";
-import { Box} from "@mui/material";
+import { Clear, Search } from "@material-ui/icons";
+import { Box } from "@mui/material";
 import Fab from "@mui/material/Fab";
 import { CalendarMonth, FilterAlt, FilterAltOff } from "@mui/icons-material";
-
-
+import StatusBarComponent from "../Components/StatusBarComponent";
 ModuleRegistry.registerModules([
   ClientSideRowModelModule,
   SetFilterModule,
@@ -62,9 +58,6 @@ export default function TableComponent(props) {
   const onGridReady = useCallback((params) => {
     setGridApi(params);
   }, []);
-
-
-  
 
   const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
 
@@ -109,11 +102,12 @@ export default function TableComponent(props) {
   const statusBar = useMemo(() => {
     return {
       statusPanels: [
-        { statusPanel: 'agTotalAndFilteredRowCountComponent', align: 'left' },
-        { statusPanel: 'agTotalRowCountComponent', align: 'center' },
-        { statusPanel: 'agFilteredRowCountComponent' },
-        { statusPanel: 'agSelectedRowCountComponent' },
-        { statusPanel: 'agAggregationComponent' },
+        { statusPanel: "agTotalAndFilteredRowCountComponent", align: "left" },
+        { statusPanel: "agTotalRowCountComponent", align: "left" },
+        { statusPanel: "agFilteredRowCountComponent",align: "center" },
+        { statusPanel: "agSelectedRowCountComponent",align: "center" },
+        { statusPanel: "agAggregationComponent",align: "center" },
+        { statusPanel: StatusBarComponent, align: "center" },
       ],
     };
   }, []);
@@ -172,98 +166,105 @@ export default function TableComponent(props) {
   return (
     <div style={{ containerStyle }} className="themeContainer">
       <Box className="p-1 mt-1 my-md-0 filterTabs text-end text-md-center">
-      {props.datefilter? <>
+        {props.datefilter ? (
+          <>
+            <Fab
+              color="transparent"
+              aria-label="Date"
+              variant="extended"
+              className="fabCustom mb-1"
+            >
+              {date ? (
+                <Clear onClick={onChangeDate} className="theme_text" />
+              ) : (
+                <CalendarMonth onClick={onChangeDate} className="theme_text" />
+              )}
+              <div className={`p-1 ${date ? "d-inline-flex" : "d-none"}`}>
+                <div className="m-1">
+                  {/* <p className="theme_text me-1 my-auto"> From </p> */}
+                  <input
+                    type="date"
+                    id="startDate"
+                    onChange={(e) => setStartDate(e.target.value)}
+                    className="px-1 btn_theme"
+                    style={props.screenWidth < 400 ? { width: 125 } : null}
+                  />
+                </div>
+                <div className="m-1">
+                  {/* <p className="theme_text me-1 my-auto"> To </p> */}
+                  <input
+                    type="date"
+                    id="endDate"
+                    onChange={(e) => setEndDate(e.target.value)}
+                    className="px-1 btn_theme"
+                    style={props.screenWidth < 400 ? { width: 125 } : null}
+                  />
+                </div>
+              </div>
+            </Fab>
+          </>
+        ) : null}
         <Fab
-          color="transparent"
-          aria-label="Date"
+          color="neutral"
+          aria-label="edit"
           variant="extended"
-          className="fabCustom mb-1"
+          className="ms-2 fabCustom mb-1"
         >
-          {date ?
-            <Clear onClick={onChangeDate} className="theme_text" />
-            :
-            <CalendarMonth onClick={onChangeDate} className="theme_text" />
-            }
-            <div className={`p-1 ${date ? "d-inline-flex" : "d-none"}`}>
-              <div className="m-1">
-                {/* <p className="theme_text me-1 my-auto"> From </p> */}
-                <input
-                  type="date"
-                  id="startDate"
-                  onChange={(e) => setStartDate(e.target.value)}
-                  className="px-1 btn_theme"
-                  style={props.screenWidth < 400 ? { width: 125 } : null}
-                />
-              </div>
-              <div className="m-1">
-                {/* <p className="theme_text me-1 my-auto"> To </p> */}
-                <input
-                  type="date"
-                  id="endDate"
-                  onChange={(e) => setEndDate(e.target.value)}
-                  className="px-1 btn_theme"
-                  style={props.screenWidth < 400 ? { width: 125 } : null}
-                />
-              </div>
-            </div>
-          </Fab>
-          </>:
-          null}
-          <Fab
-            color="neutral"
-            aria-label="edit"
-            variant="extended"
-            className="ms-2 mb-1"
-          >
-            <Search onClick={onChangeSearch} className='theme_text' />
-            <div className={`px-2 ${search ? "d-block" : "d-none"}`}>
-              <input
-                className="form-control"
-                type="search"
-                placeholder="Search..."
-                aria-label="Search"
-                onInput={onFilterTextBoxChanged}
-                id="filter-text-box"
-              />
-            </div>
-          </Fab>
-          <Fab
-            color="neutral"
-            variant="extended"
-            className="ms-2 hover"
-            onClick={filterview}
-          >
-            <FilterAlt sx={{ mr: 1 }} className="theme_text" />
-            <div className="onHover"> More Filters</div>
-          </Fab>
-          <Fab
-            color="neutral"
-            variant="extended"
-            className="ms-2 hover"
-            onClick={() => {
-              if (gridApi) {
-                for (let i in props.columnDefs) {
-                  gridApi.api
-                    .getFilterInstance(props.columnDefs[i].field)
-                    .setModel(null);
-                  gridApi.api.onFilterChanged();
-                  gridRef.current.api.setQuickFilter("");
-                  document.getElementById("filter-text-box").value = "";
-                }
+          {search ? (
+            <Clear onClick={onChangeSearch} className="theme_text" />
+          ) : (
+            <Search onClick={onChangeSearch} className="theme_text" />
+          )}
+          <div className={`px-2 ${search ? "d-block" : "d-none"}`}>
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search..."
+              aria-label="Search"
+              onInput={onFilterTextBoxChanged}
+              id="filter-text-box"
+            />
+          </div>
+        </Fab>
+        <Fab
+          color="neutral"
+          variant="extended"
+          className="ms-2 fabCustom hover"
+          onClick={filterview}
+        >
+          <FilterAlt sx={{ mr: 1 }} className="theme_text" />
+          <div className="onHover"> More Filters</div>
+        </Fab>
+        <Fab
+          color="neutral"
+          variant="extended"
+          className="ms-2 fabCustom hover"
+          onClick={() => {
+            if (gridApi) {
+              for (let i in props.columnDefs) {
+                gridApi.api
+                  .getFilterInstance(props.columnDefs[i].field)
+                  .setModel(null);
+                gridApi.api.onFilterChanged();
+                gridRef.current.api.setQuickFilter("");
+                document.getElementById("filter-text-box").value = "";
               }
-            }}
-          >
-            <FilterAltOff sx={{ mr: 1 }} className="theme_text" />
-            <div className="onHover">Reset</div>
-          </Fab>
-        </Box>
-        {/* {props.screenWidth < 770 ? (
+            }
+          }}
+        >
+          <FilterAltOff sx={{ mr: 1 }} className="theme_text" />
+          <div className="onHover">Reset</div>
+        </Fab>
+      </Box>
+      {/* {props.screenWidth < 770 ? (
           <div>
             <button onClick={filterview}>Filter</button>
           </div>
         ) : null} */}
       <div
-        className={`ag-theme-alpine ${props.screenWidth > 770 ? 'height_' : 'heightresponsive'}`}
+        className={`ag-theme-alpine ${
+          props.screenWidth > 770 ? "height_" : "heightresponsive"
+        }`}
         style={{ width: "100%", gridStyle }}
       >
         <AgGridReact
@@ -277,16 +278,14 @@ export default function TableComponent(props) {
           sideBar={props.screenWidth < 770 ? mobileSidebar() : sidebar()}
           masterDetail={true}
           detailCellRenderer={detailCellRenderer}
-          statusBar = {statusBar}
+          statusBar={statusBar}
           // onFirstDataRendered={headerHeightSetter}
           // onColumnResized={headerHeightSetter}
           // overlayLoadingTemplate={'<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>'}
           // overlayNoRowsTemplate={'<span class="ag-overlay-loading-center"><i className="fas fa-hourglass-half" style="color: blue; height: 0%"> Please wait while your data are loading </i> </span>'}
           onGridReady={onGridReady}
-        
-
         />
       </div>
-    </div >
+    </div>
   );
 }
